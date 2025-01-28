45c60c0b8f15ff30cd6d70cafbc203264eded211
unknown <chetanpinjarkar@gmail.com>
2025-01-28 10:21:58 +0000

    updated the instace creation files

--- day1/variable.tf
+++ day1/variable.tf
@@ -1,16 +1,16 @@
 variable "ami_id" {
-    description = "inserting ami value to maint.tf"
-    type = string
-    default = "ami-0b7207e48d1b6c06f"
-  
+  description = "inserting ami value to maint.tf"
+  type        = string
+  default     = "ami-0b7207e48d1b6c06f"
+
 }
 variable "type" {
-    type = string
-    default = ""
-  
+  type    = string
+  default = "t2.micro"
+
 }
 variable "keyname" {
-    type = string
-    default = ""
-  
+  type    = string
+  default = "aws2"
+
 }
