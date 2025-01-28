0c8bd7acbe39eab5be77a29f6dde230abbc0a94c
unknown <chetanpinjarkar@gmail.com>
2025-01-28 09:48:09 +0000

    Instance creation

---
+++ day1/variable.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+    description = "inserting ami value to maint.tf"
+    type = string
+    default = "ami-0b7207e48d1b6c06f"
+  
+}
+variable "type" {
+    type = string
+    default = ""
+  
+}
+variable "keyname" {
+    type = string
+    default = ""
+  
+}
