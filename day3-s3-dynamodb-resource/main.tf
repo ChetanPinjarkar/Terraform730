c5e358c19f114dbe5b1e2adab4f158ffa97191a3
unknown <chetanpinjarkar@gmail.com>
2025-02-03 12:20:40 +0000

    fmt

--- day3-s3-dynamodb-resource/main.tf
+++ day3-s3-dynamodb-resource/main.tf
@@ -1,15 +1,14 @@
 provider "aws" {
-  
 }
 
 resource "aws_s3_bucket" "myBucket" {
-    bucket = "chetankis3bucket"
+  bucket = "chetankis3bucket"
 }
 
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
-  hash_key = "LockID"
-  read_capacity = 20
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
   write_capacity = 20
   attribute {
     name = "LockID"
