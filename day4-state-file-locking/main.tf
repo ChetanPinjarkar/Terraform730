765d356ad89f150a755ec280be53ae2ce3d50eca
unknown <chetanpinjarkar@gmail.com>
2025-02-15 20:09:11 +0000

    day4 state file on mongodb

--- day4-state-file-locking/main.tf
+++ day4-state-file-locking/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "state-lock" {
-    ami = "ami-00bb6a80f01f03502"
-    instance_type = "t2.micro"
-    key_name = "aws2"
-    availability_zone = "ap-south-1a"
-    tags = {
-      Name="Ubuntu-state-lock"
-    }
+  ami               = "ami-00bb6a80f01f03502"
+  instance_type     = "t2.micro"
+  key_name          = "aws2"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "Ubuntu-state-lock"
+  }
 }
